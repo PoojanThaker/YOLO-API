d5fb9168bf79b45ed5255f2c3a30d574aa42523e
Poojan <poojanthaker@gmail.com>
2021-05-30 19:58:06 +0530

    Add home page

--- app.py
+++ app.py
@@ -22,6 +22,11 @@ def main():
     return jsonify(res)
 
 
+@app.route('/')
+def main():
+    return 'Hello'
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
